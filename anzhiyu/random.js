6095c27508e81ddf85533069b79d082b20120c9a
xie <1831668849@qq.com>
2025-10-29 13:09:58 +0800

    Site updated: 2025-10-29 13:09:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/52754888.html"];function toRandomPost(){
+var posts=["posts/1.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
