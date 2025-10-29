e134f665667e693b307fc6ea11285d73dbbb1793
xie <1831668849@qq.com>
2025-10-29 22:46:51 +0800

    Site updated: 2025-10-29 22:46:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/1.html"];function toRandomPost(){
+var posts=["posts/1.html","posts/a8c5676e.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
