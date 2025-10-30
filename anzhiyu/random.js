96d90dc478784abd23e826383e7ea3e0274c946d
xie <1831668849@qq.com>
2025-10-30 20:57:26 +0800

    Site updated: 2025-10-30 20:57:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/a8c5676e.html","posts/1.html"];function toRandomPost(){
+var posts=["posts/a8c5676e.html","posts/f3a883b3.html","posts/1.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
