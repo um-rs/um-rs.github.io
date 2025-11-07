f4d837736d861b3d19ac077f0a10427b4013f4b2
xie <1831668849@qq.com>
2025-11-07 22:39:10 +0800

    Site updated: 2025-11-07 22:39:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/6ad3431c.html","posts/f3a883b3.html","posts/a8c5676e.html","posts/b5b7f64d.html","posts/1.html"];function toRandomPost(){
+var posts=["posts/a8c5676e.html","posts/f3a883b3.html","posts/6ad3431c.html","posts/1.html","posts/b5b7f64d.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
