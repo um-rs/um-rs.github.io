57c0c3c026f81ba4c5e321a5f8c1d9d2caea2b53
xie <1831668849@qq.com>
2025-11-17 09:26:05 +0800

    Site updated: 2025-11-17 09:26:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/a8c5676e.html","posts/f3a883b3.html","posts/1acea302.html","posts/b0c76b89.html","posts/11e210a8.html","posts/b5b7f64d.html","posts/95ac3455.html","posts/6ad3431c.html","posts/aa07b340.html","posts/87b0cec.html","posts/1.html"];function toRandomPost(){
+var posts=["posts/a8c5676e.html","posts/b0c76b89.html","posts/f3a883b3.html","posts/95ac3455.html","posts/87b0cec.html","posts/11e210a8.html","posts/1.html","posts/b5b7f64d.html","posts/1acea302.html","posts/aa07b340.html","posts/6ad3431c.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
