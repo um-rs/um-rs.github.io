4cbf5f7af0b7104a0f565e251eacd89d4fdb5f5e
xie <1831668849@qq.com>
2025-11-22 15:25:39 +0800

    Site updated: 2025-11-22 15:25:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/1acea302.html","posts/a8c5676e.html","posts/f3a883b3.html","posts/b0c76b89.html","posts/95ac3455.html","posts/87199bbb.html","posts/87b0cec.html","posts/df7a8bed.html","posts/11e210a8.html","posts/1.html","posts/b5b7f64d.html","posts/6ad3431c.html","posts/b771e7ca.html","posts/aa07b340.html"];function toRandomPost(){
+var posts=["posts/a8c5676e.html","posts/f3a883b3.html","posts/1acea302.html","posts/b0c76b89.html","posts/87b0cec.html","posts/95ac3455.html","posts/df7a8bed.html","posts/b771e7ca.html","posts/11e210a8.html","posts/b5b7f64d.html","posts/6ad3431c.html","posts/aa07b340.html","posts/87199bbb.html","posts/1.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
